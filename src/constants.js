df6b079951a43e269f9a4fbcba4b77627b4eb407
timothy-okoduwa <timmyleeokoduwa7@gmail.com>
2025-10-03 01:36:41 +0100

    new shii

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5001' : '';
-export const BASE_URL = process.env.REACT_APP_API_URL || "";
+export const BASE_URL = "https://jojo-backend-adz9.onrender.com" || "";
 export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
 export const ORDERS_URL = "/api/orders";
