d65a53b119cccb5791ed462872f68fbd7f097641
timothy-okoduwa <timmyleeokoduwa7@gmail.com>
2025-10-03 01:27:46 +0100

    new env

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5001' : '';
-export const BASE_URL = ''; // If using proxy
-export const PRODUCTS_URL = '/api/products';
-export const USERS_URL = '/api/users';
-export const ORDERS_URL = '/api/orders';
-export const PAYSTACK_URL = '/api/config/paystack';
+export const BASE_URL = process.env.REACT_APP_API_URL || "";
+export const PRODUCTS_URL = "/api/products";
+export const USERS_URL = "/api/users";
+export const ORDERS_URL = "/api/orders";
+export const PAYSTACK_URL = "/api/config/paystack";
